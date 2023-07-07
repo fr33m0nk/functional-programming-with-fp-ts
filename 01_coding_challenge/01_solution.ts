2e8e01c72b267e3e96dc45bdd4f63ce6d01bf53d
fr33m0nk <prashantsinha@protonmail.com>
2023-07-07 14:07:39 +0530

    Improve solution to Problem 1

--- 01_coding_challenge/01_solution.ts
+++ 01_coding_challenge/01_solution.ts
@@ -1,6 +1,9 @@
 // Note that parameter1 and parameter2 are just placeholders
 //you have to set their types and also can change their names.
-type ShowResult = (status: string, score: number) => void
+
+type Status = 'SUCCESS' | 'FAILED'
+
+type ShowResult = (status: Status, score: number) => void
 const showResult: ShowResult = (status, score) =>
   (status === 'SUCCESS' && score === 5) ?
     console.log('Great, you got the highest possible score') :
